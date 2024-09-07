d152f01ff7ac424ce98c4c5c6a0ec3dac90e2b92
Void-11 <vivaanmishra777@gmail.com>
2024-09-07 23:14:37 +0530

    Implemented Enemy Lock

---
+++ Source/MeleeCombat/Private/Combat/Components/LockOnComponent.cpp
@@ -0,0 +1,121 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Combat/Components/LockOnComponent.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+// Sets default values for this component's properties
+ULockOnComponent::ULockOnComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void ULockOnComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	OwnerRef = GetOwner<ACharacter>();
+	Controller = GetWorld()->GetFirstPlayerController();
+	MovementComp = OwnerRef->GetCharacterMovement();
+	SpringArmComp = OwnerRef->FindComponentByClass<USpringArmComponent>();
+	
+}
+
+void ULockOnComponent::StartLockOn(float Radius)
+{
+	FHitResult OutResult;
+	FVector CurrentLocation{ OwnerRef->GetActorLocation() };
+	FCollisionShape Sphere{ FCollisionShape::MakeSphere(Radius) };
+	FCollisionQueryParams IgnorePrams{ 
+		FName(TEXT("Ignore Query Params")),
+		false,
+		OwnerRef
+	};
+
+	bool bHasFoundTarget{ GetWorld()->SweepSingleByChannel(
+		OutResult,
+		CurrentLocation,
+		CurrentLocation,
+		FQuat::Identity,
+		ECollisionChannel::ECC_GameTraceChannel1,
+		Sphere,
+		IgnorePrams
+	) };
+
+	if (!bHasFoundTarget) { return; }
+
+	CurrentTargetActor = OutResult.GetActor();
+
+	Controller->SetIgnoreLookInput(true);
+	MovementComp->bOrientRotationToMovement = false;
+	MovementComp->bUseControllerDesiredRotation = true;
+
+	SpringArmComp->TargetOffset = FVector{ 0.0, 0.0, 100.0 };
+
+}
+
+void ULockOnComponent::EndLockOn()
+{
+	CurrentTargetActor = nullptr;
+
+	MovementComp->bOrientRotationToMovement = true;
+	MovementComp->bUseControllerDesiredRotation = false;
+	SpringArmComp->TargetOffset = FVector::ZeroVector;
+
+	Controller->ResetIgnoreLookInput();
+}
+
+void ULockOnComponent::ToggleLockOn(float Radius)
+{
+	if (IsValid(CurrentTargetActor))
+	{
+		EndLockOn();
+	}
+	else
+	{
+		StartLockOn(Radius);
+	}
+}
+
+
+// Called every frame
+void ULockOnComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	if (!IsValid(CurrentTargetActor)) { return; }
+
+	FVector CurrentLocation{ OwnerRef->GetActorLocation() };
+	FVector TargetLocation{ CurrentTargetActor->GetActorLocation() };
+
+	double TargetDistance{ 
+		FVector::Distance(CurrentLocation, TargetLocation) 
+	};
+
+	if (TargetDistance >= BreakDistance) 
+	{
+		EndLockOn();
+		return;
+	}
+
+	TargetLocation.Z -= 125;
+
+	FRotator NewRotation{ UKismetMathLibrary::FindLookAtRotation(
+		CurrentLocation, TargetLocation
+	) };
+
+	Controller->SetControlRotation(NewRotation);
+
+}
+
---
+++ Source/MeleeCombat/Public/Combat/Components/LockOnComponent.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "LockOnComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class MELEECOMBAT_API ULockOnComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+	ACharacter* OwnerRef;
+	APlayerController* Controller;
+	class UCharacterMovementComponent* MovementComp;
+
+	class USpringArmComponent* SpringArmComp;
+
+public:	
+	// Sets default values for this component's properties
+	ULockOnComponent();
+	
+	AActor* CurrentTargetActor;
+	
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void StartLockOn(float Radius = 750.0f);
+
+	void EndLockOn();
+
+	UFUNCTION(BlueprintCallable)
+	void ToggleLockOn(float Radius = 750.0f);
+
+	UPROPERTY(EditAnywhere)
+	double BreakDistance{ 1000.0 };
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+	
+};
